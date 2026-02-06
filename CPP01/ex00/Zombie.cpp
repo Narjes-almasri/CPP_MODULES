74c64d4b6a5716b1d3c3409fa3be2d44baa973f9
Narjes Almasri <narjesMohammad999@gmail.com>
2026-02-06 20:35:00 +0300

    edited

--- CPP01/ex00/Zombie.cpp
+++ CPP01/ex00/Zombie.cpp
@@ -6,7 +6,7 @@
 /*   By: naalmasr <naalmasr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/18 19:57:52 by naalmasr          #+#    #+#             */
-/*   Updated: 2025/10/18 19:57:53 by naalmasr         ###   ########.fr       */
+/*   Updated: 2026/02/06 20:34:41 by naalmasr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ void Zombie::announce(void)
 {
 	std::cout<<"BraiiiiiiinnnzzzZ..."<<"\n";
 }
-Zombie::Zombie(std::string name) //change later
+Zombie::Zombie(std::string name) 
 {
 	std::cout<<"Zombie " << name << "  is created \n" ;
 }
